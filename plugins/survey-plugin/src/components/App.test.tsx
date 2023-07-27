f8029c974a61d947c79565d1083fd154312dd9fc
foodproduct <dcbarnez@gmail.com>
2023-07-27 15:20:35 -0400

    mock fetching for tests

--- plugins/survey-plugin/src/components/App.test.tsx
+++ plugins/survey-plugin/src/components/App.test.tsx
@@ -1,10 +1,19 @@
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import App from "./App";
+
+fetchMock.mockResponse(
+  JSON.stringify({
+    value:
+      "https://docs.google.com/forms/d/e/1FAIpQLSd068wYDvfxbhB75fTx-KM7aWb9gNiLLcnjA6SQ4ulT9SLgqA/viewform?embedded=true",
+  })
+);
 
 describe("App", () => {
   it("indicates that it's an awesome plugin", () => {
     render(<App />);
 
-    expect(screen.queryByText(/My Awesome Cortex Plugin/)).toBeInTheDocument();
+    expect(fetch).toHaveBeenCalledWith(
+      "https://api.cortex.dev/catalog/inventory-planner/custom-data/survey"
+    );
   });
 });
