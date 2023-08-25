43443bcf1dc43d3b0045c9bbf2e973e69f81c985
foodproduct <dcbarnez@gmail.com>
2023-08-25 18:07:50 -0400

    use octokit for fetching from GitHub

--- plugins/github-releases/src/api/Github.ts
+++ plugins/github-releases/src/api/Github.ts
@@ -1,13 +1,24 @@
+import { Octokit } from "octokit";
+
+const octokit = new Octokit();
+
 export const getReleases = async (
   owner: string,
   repo: string,
   { perPage = 100, page = 1 }
 ): Promise<any> => {
-  const res = await fetch(
-    `https://api.github.com/repos/${owner}/${repo}/releases?per_page=${perPage}&page=${page}`
-  );
-  if (res.status >= 400 && res.status < 600) {
-    throw new Error(`Bad response from server: ${await res.text()}`);
+  const releases = await octokit.rest.repos.listReleases({
+    owner,
+    repo,
+    query: {
+      per_page: perPage,
+      page,
+    },
+  });
+
+  if (typeof releases.data === "string") {
+    return JSON.parse(releases.data);
   }
-  return await res.json();
+
+  return releases.data;
 };
