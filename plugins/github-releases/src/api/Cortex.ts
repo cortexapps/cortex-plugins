800711eab2c357302a870b1e27329e954b0baa73
foodproduct <dcbarnez@gmail.com>
2023-08-28 09:21:08 -0700

    delete unused method

--- plugins/github-releases/src/api/Cortex.ts
+++ plugins/github-releases/src/api/Cortex.ts
@@ -1,15 +1,8 @@
-import { CortexApi, type CortexContextResponse } from "@cortexapps/plugin-core";
-
-export const getCortexContext = async (): Promise<CortexContextResponse> => {
-  const context = await CortexApi.getContext();
-
-  return context;
-};
-
 export const getEntityYaml = async (
   baseUrl: string,
   entityTag: string
 ): Promise<any> => {
   const res = await fetch(`${baseUrl}/catalog/${entityTag}/openapi`);
+
   return await res.json();
 };
