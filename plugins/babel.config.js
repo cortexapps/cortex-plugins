24933b674638f558fcffe2bc524b742c0f682e71
Fernando Cremer <fernandocremer@Fernandos-MacBook-Pro.local>
2023-09-26 14:34:14 -0400

    clean up

--- plugins/babel.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  plugins: ["@babel/plugin-syntax-jsx"],
-  presets: [
-    ["@babel/preset-env", { targets: { node: "current" } }],
-    "@babel/preset-typescript",
-    ["@babel/preset-react", { runtime: "automatic" }],
-  ],
-};
