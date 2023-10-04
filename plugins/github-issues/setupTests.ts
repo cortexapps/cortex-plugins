50e5313d71693c3a70991c490732989a1b465aaa
Fernando Cremer <fernandocremer@Fernandos-MacBook-Pro.local>
2023-10-04 16:10:29 -0400

    clean up

--- plugins/github-issues/setupTests.ts
+++ plugins/github-issues/setupTests.ts
@@ -2,7 +2,6 @@ import "@testing-library/jest-dom/extend-expect";
 import fetchMock from "jest-fetch-mock";
 
 fetchMock.enableMocks();
-
 const mockContext = {
   apiBaseUrl: "https://api.getcortexapp.com",
   entity: {
--- plugins/github-issues/src/components/Issues.tsx
+++ plugins/github-issues/src/components/Issues.tsx
@@ -6,10 +6,8 @@ import {
   Text,
   Loader,
   usePluginContext,
-  
 } from "@cortexapps/plugin-core/components";
 import "../baseStyles.css";
-
 
 // Set your Github url. Cloud is https://api.github.com
 const ghURL = `https://api.github.com/`;
@@ -20,45 +18,42 @@ const Issues: React.FC = () => {
   console.log(context);
   const [posts, setPosts] = React.useState<any[]>([]);
   const [isLoading, setIsLoading] = React.useState(
-    context.location === PluginContextLocation.Entity );
+    context.location === PluginContextLocation.Entity
+  );
   React.useEffect(() => {
     const fetchData = async (): Promise<void> => {
       // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
       const cortexTag = context.entity!.tag;
       const cortexURL = context.apiBaseUrl;
-      const serviceResult = await fetch(`${cortexURL}/catalog/${cortexTag}/openapi`);
+      const serviceResult = await fetch(
+        `${cortexURL}/catalog/${cortexTag}/openapi`
+      );
       const serviceJson = await serviceResult.json();
       try {
-        if (serviceJson.info?.["x-cortex-git"].github.repository !== undefined) {
+        if (
+          serviceJson.info?.["x-cortex-git"].github.repository !== undefined
+        ) {
           hasGitHub = true;
           // If we have a GitHub tag, we assume there is a repo defined, let's get the value
           const ghRepo: string =
-          serviceJson.info["x-cortex-git"].github.repository;
+            serviceJson.info["x-cortex-git"].github.repository;
           // Let's check if we have a basepath defined, to check for mono repo
-          let issueURL:string = "";
+          let issueURL: string = "";
           if (serviceJson.info["x-cortex-git"].github.basepath !== undefined) {
             // we are going to assume that each service is being tracked via labels
             issueURL = `${ghURL}repos/${ghRepo}/issues?labels=${cortexTag}`;
-          }
-          else
-          {
-            issueURL =`${ghURL}repos/${ghRepo}/issues?direction=asc`;
+          } else {
+            issueURL = `${ghURL}repos/${ghRepo}/issues?direction=asc`;
           }
           const issuesResult = await fetch(issueURL);
           const issuesJson = await issuesResult.json();
-          if (issuesJson.length > 0)
-            {
-              hasIssues = true;
-              setPosts(issuesJson);  
-            }
+          if (issuesJson.length > 0) {
+            hasIssues = true;
+            setPosts(issuesJson);
           }
-      }      
-      catch(Error){
-        alert(Error.message)
-
-      }
+        }
+      } catch (Error) {}
       setIsLoading(false);
-      
     };
     void fetchData();
   }, []);
@@ -103,18 +98,18 @@ const Issues: React.FC = () => {
   if (hasGitHub) {
     return isLoading ? (
       <Loader />
-    ) : (<SimpleTable config={config} items={posts} /> );
+    ) : (
+      <SimpleTable config={config} items={posts} />
+    );
   } else if (hasGitHub && !hasIssues) {
     return isLoading ? (
       <Loader />
     ) : (
       <Box backgroundColor="light" padding={3} borderRadius={2}>
-        <Text>
-          We could not find any Issues associated to this Service
-        </Text>
+        <Text>We could not find any Issues associated to this Service</Text>
       </Box>
     );
-  }  else {
+  } else {
     return isLoading ? (
       <Loader />
     ) : (
