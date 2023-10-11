c65acc168f0c4d86dadb5556ec9217ba67d5b0d3
Erik James <erik.james@cortex.io>
2023-10-11 12:07:18 -0700

    Cleaning up formatting

--- plugins/harness-cicd-plugin/src/components/Stage.tsx
+++ plugins/harness-cicd-plugin/src/components/Stage.tsx
@@ -16,15 +16,11 @@ import "../baseStyles.css";
 
 
 const forceReloadDecorator = () => {
-  
     window.location.reload();
-  console.log("nothing");
-
-  
+    console.log("nothing");
 };
 
 const nonCortexPOST = (url: string, body:string) => {
-    
     fetch(url, {
         method: 'POST',
         headers: {
@@ -45,13 +41,6 @@ const Stage: React.FC = () => {
     context.location === PluginContextLocation.Entity );
   React.useEffect(() => {
       const fetchData = async (): Promise<void> => {
-        // let runOnce = false;
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-
-      // const context = usePluginContext();
-      // const secret = context.user!.secrets.harness-account-identifier;
-
-
       let anyRunning = false;
       
       let configData = require('../assets/config.json');
@@ -65,114 +54,109 @@ const Stage: React.FC = () => {
 
       const harnessURL = `https://app.harness.io`;
       try{
-
-
-      const orgRequest = await CortexApi.proxyFetch(
-        `${harnessURL}/v1/orgs?`
-      );
-      const orgResult = await orgRequest.json();
-      
-      var totalExecutions : object[] = [];
-      var iOrg;
-
-      // comm
-
-      for(iOrg in orgResult){
-          let orgIdentifier = orgResult[iOrg].org.identifier;
-
-          const projectRequest = await CortexApi.proxyFetch(
-          `${harnessURL}/v1/orgs/default/projects?`
-        );
-
-        const projectResult = await projectRequest.json();
-
-        var iProj;
-
-        for(iProj in projectResult){
-          let projectIdentifier = projectResult[iProj].project.identifier;
-          console.log('>>>>>>> >>>>>>.>>> projectIdentifier = ' + projectIdentifier);
-
-          const pipelineRequest = await CortexApi.proxyFetch(
-            `${harnessURL}/v1/orgs/${orgIdentifier}/projects/${projectIdentifier}/pipelines?`
+          const orgRequest = await CortexApi.proxyFetch(
+            `${harnessURL}/v1/orgs?`
           );
-
-          const pipelineResult = await pipelineRequest.json();
-
-          var a;
+          const orgResult = await orgRequest.json();
           
-          var index = 1;
-          
-          for (a in pipelineResult) {
+          var totalExecutions : object[] = [];
+          var iOrg;
 
-            let pipelineFetchInputYamlResult;
+          for(iOrg in orgResult){
+              let orgIdentifier = orgResult[iOrg].org.identifier;
 
-            if (accountIDAvailable) {
+              const projectRequest = await CortexApi.proxyFetch(
+              `${harnessURL}/v1/orgs/default/projects?`
+            );
 
-              const pipelineInputSetRequest = await CortexApi.proxyFetch(
-              `${harnessURL}/pipeline/api/inputSets?pageIndex=0&pageSize=100&accountIdentifier=${accountIdentifier}&orgIdentifier=${orgIdentifier}&projectIdentifier=${projectIdentifier}&pipelineIdentifier=${pipelineResult[a].identifier}`
+            const projectResult = await projectRequest.json();
+
+            var iProj;
+
+            for(iProj in projectResult){
+              let projectIdentifier = projectResult[iProj].project.identifier;
+              console.log('>>>>>>> >>>>>>.>>> projectIdentifier = ' + projectIdentifier);
+
+              const pipelineRequest = await CortexApi.proxyFetch(
+                `${harnessURL}/v1/orgs/${orgIdentifier}/projects/${projectIdentifier}/pipelines?`
               );
 
-              const pipelineInputSetResult = await pipelineInputSetRequest.json();
+              const pipelineResult = await pipelineRequest.json();
 
-              var c;
-              var pipelineInputSet;
-              for(c in pipelineInputSetResult.data.content){
-                console.log('>>>>>>>>>  >>>>> >> pipelineInputSetResult is ' + JSON.stringify(pipelineInputSetResult.data.content[c].name));
-                pipelineInputSet = pipelineInputSetResult.data.content[c].name;
-              }
-
-              const pipelineFetchInputYamlRequest = await CortexApi.proxyFetch(
-              `${harnessURL}/pipeline/api/inputSets/${pipelineInputSet}?accountIdentifier=${accountIdentifier}&orgIdentifier=${orgIdentifier}&projectIdentifier=${projectIdentifier}&pipelineIdentifier=${pipelineResult[a].identifier}`
-              );
-
-              pipelineFetchInputYamlResult = await pipelineFetchInputYamlRequest.json();
-
-            }
-
-
-            var b;
-            for(b in pipelineResult[a].recent_execution_info){
-              let isRunning = pipelineResult[a].recent_execution_info[b].execution_status.toLowerCase() == "running";
-
-              if (isRunning){
-                anyRunning = true;
-              }
-
-              var thisPipeline = {rerun:{status:"", url:"", body:"", available: accountIDAvailable}, no:index++, name:{pipelineID:pipelineResult[a].identifier, pipelineName:pipelineResult[a].name, runID:pipelineResult[a].recent_execution_info[b].execution_id}, status:"", details:"", time:{start: new Date(), end:new Date(), available:!isRunning}};
-
+              var a;
               
-              thisPipeline.status = pipelineResult[a].recent_execution_info[b].execution_status;
-             
-
-              if (accountIDAvailable) {
-                let inputURL = `${harnessURL}/pipeline/api/pipeline/execute/${pipelineResult[a].identifier}/inputSetList?accountIdentifier=${accountIdentifier}&orgIdentifier=${orgIdentifier}&projectIdentifier=${projectIdentifier}`
-                let inputBody = JSON.stringify({
-                           inputSetReferences: [pipelineInputSet],
-                           withMergedPipelineYaml: true,
-                           stageIdentifiers: ["string"],
-                           lastYamlToMerge: pipelineFetchInputYamlResult.data.inputSetYaml
-                        });
-
-                 thisPipeline.rerun = {status: thisPipeline.status, url:inputURL,body: inputBody, available:!isRunning};
-              }
-
+              var index = 1;
               
-              thisPipeline.time.start = new Date(pipelineResult[a].recent_execution_info[b].started);
-              thisPipeline.time.end = new Date(pipelineResult[a].recent_execution_info[b].ended);
-              totalExecutions.push(thisPipeline);
-            }
+              for (a in pipelineResult) {
 
-            
+                let pipelineFetchInputYamlResult;
+
+                if (accountIDAvailable) {
+
+                  const pipelineInputSetRequest = await CortexApi.proxyFetch(
+                    `${harnessURL}/pipeline/api/inputSets?pageIndex=0&pageSize=100&accountIdentifier=${accountIdentifier}&orgIdentifier=${orgIdentifier}&projectIdentifier=${projectIdentifier}&pipelineIdentifier=${pipelineResult[a].identifier}`
+                  );
+
+                  const pipelineInputSetResult = await pipelineInputSetRequest.json();
+
+                  var c;
+                  var pipelineInputSet;
+                  for(c in pipelineInputSetResult.data.content){
+                    pipelineInputSet = pipelineInputSetResult.data.content[c].name;
+                  }
+
+                  const pipelineFetchInputYamlRequest = await CortexApi.proxyFetch(
+                    `${harnessURL}/pipeline/api/inputSets/${pipelineInputSet}?accountIdentifier=${accountIdentifier}&orgIdentifier=${orgIdentifier}&projectIdentifier=${projectIdentifier}&pipelineIdentifier=${pipelineResult[a].identifier}`
+                  );
+
+                  pipelineFetchInputYamlResult = await pipelineFetchInputYamlRequest.json();
+
+                }
+
+
+                var b;
+                for(b in pipelineResult[a].recent_execution_info){
+                  let isRunning = pipelineResult[a].recent_execution_info[b].execution_status.toLowerCase() == "running";
+
+                  if (isRunning){
+                    anyRunning = true;
+                  }
+
+                  var thisPipeline = {rerun:{status:"", url:"", body:"", available: accountIDAvailable}, no:index++, name:{pipelineID:pipelineResult[a].identifier, pipelineName:pipelineResult[a].name, runID:pipelineResult[a].recent_execution_info[b].execution_id}, status:"", details:"", time:{start: new Date(), end:new Date(), available:!isRunning}};
+
+                  
+                  thisPipeline.status = pipelineResult[a].recent_execution_info[b].execution_status;
+                 
+
+                  if (accountIDAvailable) {
+                    let inputURL = `${harnessURL}/pipeline/api/pipeline/execute/${pipelineResult[a].identifier}/inputSetList?accountIdentifier=${accountIdentifier}&orgIdentifier=${orgIdentifier}&projectIdentifier=${projectIdentifier}`
+                    let inputBody = JSON.stringify({
+                               inputSetReferences: [pipelineInputSet],
+                               withMergedPipelineYaml: true,
+                               stageIdentifiers: ["string"],
+                               lastYamlToMerge: pipelineFetchInputYamlResult.data.inputSetYaml
+                            });
+
+                     thisPipeline.rerun = {status: thisPipeline.status, url:inputURL,body: inputBody, available:!isRunning};
+                  }
+
+                  
+                  thisPipeline.time.start = new Date(pipelineResult[a].recent_execution_info[b].started);
+                  thisPipeline.time.end = new Date(pipelineResult[a].recent_execution_info[b].ended);
+                  totalExecutions.push(thisPipeline);
+                }
+
+                
+
+              }
+            }
 
           }
-        }
 
-      }
-
-      setPosts(totalExecutions);
+          setPosts(totalExecutions);
 
        } catch (error) {
-        alert(error.message);
+          alert(error.message);
       }
 
       setIsLoading(false);
