f1cf7ff357905a6f12f807c6aa87d3e7a50453af
cremerfc <cremerfc@users.noreply.github.com>
2023-10-25 12:57:17 -0400

    Update plugins/gitlab-issues/src/api/Cortex.ts
    Co-authored-by: David Barnes <dcbarnez@gmail.com>

--- plugins/gitlab-issues/src/api/Cortex.ts
+++ plugins/gitlab-issues/src/api/Cortex.ts
@@ -1,7 +1,7 @@
 export const getEntityYaml = async (
   baseUrl: string,
   entityTag: string
-): Promise<any> => {
+): Promise<Record<string, any>> => {
   const res = await fetch(`${baseUrl}/catalog/${entityTag}/openapi`);
 
   return await res.json();
