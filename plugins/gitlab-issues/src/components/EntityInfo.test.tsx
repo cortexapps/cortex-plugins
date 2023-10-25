d17569d8dc1d775c3ab29d72160ed60648ee9507
cremerfc <cremerfc@users.noreply.github.com>
2023-10-25 12:56:58 -0400

    Update plugins/gitlab-issues/src/components/EntityInfo.test.tsx
    Co-authored-by: David Barnes <dcbarnez@gmail.com>

--- plugins/gitlab-issues/src/components/EntityInfo.test.tsx
+++ plugins/gitlab-issues/src/components/EntityInfo.test.tsx
@@ -2,7 +2,7 @@ import { render, screen, waitFor } from "@testing-library/react";
 import EntityInfo from "./EntityInfo";
 
 describe("EntityInfo", () => {
-  it("Shows message when no GitLab info found", async () => {
+  it("shows message when no GitLab info found", async () => {
     fetchMock.mockIf(
       /^https:\/\/api\.getcortexapp\.com\/catalog\/.*/,
       async (_req: Request) => {
