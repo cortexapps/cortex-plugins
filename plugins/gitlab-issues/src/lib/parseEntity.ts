31dc53b65cf20ce6626a31c46b9c0cdcff3b0ce0
foodproduct <dcbarnez@gmail.com>
2023-10-25 17:27:36 -0400

    simplify parseEntity

--- plugins/gitlab-issues/src/lib/parseEntity.ts
+++ plugins/gitlab-issues/src/lib/parseEntity.ts
@@ -1,32 +1,26 @@
 export const getGitlabDetailsFromEntity = (
   entity: Record<string, any>
 ): { owner: string; repo: string; basepath: string } | undefined => {
-  let owner = "";
-  let repo = "";
-  let basepath = "";
-
   try {
     const gitlabDetails = entity.info["x-cortex-git"]?.gitlab;
-    // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+
     if (!gitlabDetails) {
-      owner = "";
-      repo = "";
-      basepath = "";
-
       return undefined;
-    } else {
-      [owner, repo] = gitlabDetails?.repository?.split("/");
-      basepath = gitlabDetails?.repository?.basepath;
     }
-    if (!owner || !repo) {
-      owner = "";
-      repo = "";
-      basepath = "";
-    }
-  } catch (err: any) {
-    // console.log(`Error parsing GitLab details from entity descriptor:`, err);
-  }
 
-  // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
-  return { owner, repo, basepath };
+    const [owner, repo] = gitlabDetails?.repository?.split("/");
+    const basepath = gitlabDetails?.repository?.basepath;
+
+    if (!owner || !repo) {
+      return undefined;
+    }
+
+    return {
+      basepath,
+      owner,
+      repo,
+    };
+  } catch (err: any) {
+    console.error(`Error parsing GitLab details from entity descriptor:`, err);
+  }
 };
