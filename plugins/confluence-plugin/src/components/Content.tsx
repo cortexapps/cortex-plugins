00a8975c0c45986af19a47b22bc905a3757f572a
Fernando Cremer <fernandocremer@Fernandos-MacBook-Pro.local>
2024-01-26 09:25:40 -0500

    implemented David's suggested changes

--- plugins/confluence-plugin/src/components/Content.tsx
+++ plugins/confluence-plugin/src/components/Content.tsx
@@ -12,58 +12,65 @@ import { getConfluenceDetailsFromEntity } from "../lib/parseEntity";
 const baseJIRAUrl = 'https://cortex-se-test.atlassian.net';
 // const defaultPage = baseJIRAUrl + '/wiki/rest/api/content/131073?expand=body.view';
 const defaultPage = '131073';
-let pageContent:string = '';
-let pageTitle:string = '';
+
 
 const Content: React.FC = () => {
+  const [pageContent, setPageContent] = useState<string | undefined>();
+  const [pageTitle, setPageTitle] = useState<string | undefined>();
   const context = usePluginContext();
   const [entityYaml, setEntityYaml] = useState<
     Record<string, any> | undefined >();
   const [entityPage, setEntityPage] = useState<string|any>('');
   const [isLoading, setIsLoading] = useState<boolean>(true);
  
-  // If the plugin is running inside an entity, let's get the Spec and check for a confluence page id
-  if (context.location === PluginContextLocation.Entity) {           
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    const fetchEntityYaml = useCallback(async () => {
-      const entityTag = context.entity?.tag;  
-      if (!isNil(entityTag)) {
+ 
+  const fetchEntityYaml = useCallback(async () => {
+    const entityTag = context.entity?.tag;  
+    if (!isNil(entityTag)) {
         const yaml = await getEntityYaml(context.apiBaseUrl, entityTag);
         setEntityYaml(yaml);
       }
-    }, [context.apiBaseUrl, context.entity?.tag]);
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    useEffect(() => {
-      void fetchEntityYaml();
-    }, [fetchEntityYaml]);
+  }, [context.apiBaseUrl, context.entity?.tag]);
+  
+  // If the plugin is running inside an entity, let's get the Spec and check for a confluence page id
+  if (context.location === PluginContextLocation.Entity) {
     const pageID  = isEmpty(entityYaml) ? undefined : getConfluenceDetailsFromEntity(entityYaml);
+    console.log(pageID)
     if (!isNil(pageID)){
       setEntityPage(pageID)
-    }
+    }    
+    
   }
-  console.log(entityPage)
-  const fetchPageContent = async (): Promise<void> => {
-    if (isEmpty(entityPage)){
+  const fetchPageContent = useCallback(async (): Promise<void> => {
+    if (isEmpty(entityPage)) {
       setEntityPage(defaultPage);      
     }
-    const jiraURL = baseJIRAUrl + '/wiki/rest/api/content/'+ JSON.stringify(entityPage).replace(/['"]+/g, '') +'?expand=body.view'
+    const pageToRender = JSON.stringify(entityPage).replace(/['"]+/g, '')
+    const jiraURL = baseJIRAUrl + '/wiki/rest/api/content/'+ pageToRender +'?expand=body.view'
     const contentResult = await fetch(jiraURL);
     const contentJSON = await contentResult.json();
-    pageContent = contentJSON.body.view.value
-    pageTitle = contentJSON.title
+    setPageContent(contentJSON.body.view.value);
+    setPageTitle(contentJSON.title);
     setIsLoading(false);
-  };
+  },[entityPage]);
   void fetchPageContent();
+  
+  useEffect(() => {
+    const fetchThenFetch = async (): Promise<void> => {
+      await fetchEntityYaml();
+      await fetchPageContent();
+    };
+    void fetchThenFetch(); 
+  }, [fetchEntityYaml, fetchPageContent]);
 
-  return (
-   isLoading ? (
+  return    isLoading ? (
     <Loader />
   ) : (
     <div>
-      <h1 dangerouslySetInnerHTML={{__html: pageTitle}}></h1>
-      <p dangerouslySetInnerHTML={{__html: pageContent}}></p>
+      <h1 dangerouslySetInnerHTML={{__html: pageTitle as string}}></h1>
+      <p dangerouslySetInnerHTML={{__html: pageContent as string}}></p>
     </div>
-  )
   );
+  
 };
 export default Content;
