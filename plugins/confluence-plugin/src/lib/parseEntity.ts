069ceb69226a9e73f3c58e41dbabd693e193ac2a
Fernando Cremer <fernandocremer@Fernandos-MacBook-Pro.local>
2024-02-02 15:58:50 -0500

    ran yarn fix

--- plugins/confluence-plugin/src/lib/parseEntity.ts
+++ plugins/confluence-plugin/src/lib/parseEntity.ts
@@ -2,7 +2,6 @@ export const getConfluenceDetailsFromEntity = (
   entity: Record<string, any>
 ): { pageID: string } | undefined => {
   const pageID = entity.info["x-cortex-confluence"]?.pageID;
-  console.log(pageID);
   // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
   if (!pageID) {
     return undefined;
