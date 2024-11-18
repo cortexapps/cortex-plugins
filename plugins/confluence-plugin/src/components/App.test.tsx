6d278de7ff9663cecb89da7bfc7c6488fcb8e021
Martin Stone <martindstone@me.com>
2024-11-18 10:08:26 -0500

    remove comment

--- plugins/confluence-plugin/src/components/App.test.tsx
+++ plugins/confluence-plugin/src/components/App.test.tsx
@@ -1,4 +1,3 @@
-// import { render, screen, waitFor } from "@testing-library/react";
 import { render, waitFor } from "@testing-library/react";
 import fetchMock from "jest-fetch-mock";
 import App from "./App";
