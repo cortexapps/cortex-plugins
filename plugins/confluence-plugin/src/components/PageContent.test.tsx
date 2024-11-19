b78f809634076455109db29da087f6f39890301c
Martin Stone <martindstone@me.com>
2024-11-19 09:58:07 -0500

    remove hardcoded URL, add instructions, fix tests (#35)

--- plugins/confluence-plugin/src/components/PageContent.test.tsx
+++ plugins/confluence-plugin/src/components/PageContent.test.tsx
@@ -1,42 +1,7 @@
 import { render, screen, waitFor } from "@testing-library/react";
 import PageContent from "./PageContent";
 
-const mockPageContent = {
-  id: "131073",
-  type: "page",
-  status: "current",
-  title: "AppDirect Runbook",
-  macroRenderedOutput: {},
-  body: {
-    view: {
-      value:
-        '<p><style>[data-colorid=kggi8475gd]{color:#bf2600} html[data-color-mode=dark] [data-colorid=kggi8475gd]{color:#ff6640}</style><strong>This document describes what we do when it breaks!</strong></p><p><br />Step 1 - <span data-colorid="kggi8475gd">EVERYBODY</span> PANIC!!!!!!!</p><p>Step2 - Run to the hills</p><p>Step3 - Run for your lives</p><p /><h2 id="AppDirectRunbook-ArchitecturalDigram">Architectural Digram</h2><span class="confluence-embedded-file-wrapper image-center-wrapper confluence-embedded-manual-size"><img class="confluence-embedded-image image-center" alt="cloud_infrastructure (3).png" width="760" loading="lazy" src="https://cortex-se-test.atlassian.net/wiki/download/thumbnails/131073/cloud_infrastructure%20(3).png?version=1&amp;modificationDate=1706209753278&amp;cacheVersion=1&amp;api=v2&amp;width=760&amp;height=888" data-image-src="https://cortex-se-test.atlassian.net/wiki/download/attachments/131073/cloud_infrastructure%20(3).png?version=1&amp;modificationDate=1706209753278&amp;cacheVersion=1&amp;api=v2" data-height="1632" data-width="1396" data-unresolved-comment-count="0" data-linked-resource-id="2523157" data-linked-resource-version="1" data-linked-resource-type="attachment" data-linked-resource-default-alias="cloud_infrastructure (3).png" data-base-url="https://cortex-se-test.atlassian.net/wiki" data-linked-resource-content-type="image/png" data-linked-resource-container-id="131073" data-linked-resource-container-version="4" data-media-id="d04b592d-798d-486e-80af-6af581929d0b" data-media-type="file" srcset="https://cortex-se-test.atlassian.net/wiki/download/thumbnails/131073/cloud_infrastructure%20(3).png?version=1&amp;modificationDate=1706209753278&amp;cacheVersion=1&amp;api=v2&amp;width=1095&amp;height=1280 2x, https://cortex-se-test.atlassian.net/wiki/download/thumbnails/131073/cloud_infrastructure%20(3).png?version=1&amp;modificationDate=1706209753278&amp;cacheVersion=1&amp;api=v2&amp;width=760&amp;height=888 1x" /></span><p />',
-      representation: "view",
-      _expandable: {
-        webresource: "",
-        embeddedContent: "",
-        mediaToken: "",
-        content: "/rest/api/content/131073",
-      },
-    },
-    _expandable: {
-      editor: "",
-      atlas_doc_format: "",
-      export_view: "",
-      styled_view: "",
-      dynamic: "",
-      storage: "",
-      editor2: "",
-      anonymous_export_view: "",
-    },
-  },
-};
-
-const serviceYaml = {
-  info: {
-    "x-cortex-confluence": { pageID: 131073 },
-  },
-};
+import { successMockBodies, noEntityMockBodies } from "../mocks/mockBodies";
 
 describe("PageContent", () => {
   beforeEach(() => {
@@ -44,27 +9,37 @@ describe("PageContent", () => {
   });
 
   it("shows message when no page is found", async () => {
-    fetchMock.mockResponses(
-      [JSON.stringify(serviceYaml), { status: 200 }],
-      [JSON.stringify({}), { status: 500 }]
-    );
+    fetchMock.mockResponse(async (req) => {
+      const url = req.url.split("?")[0];
+      if (!noEntityMockBodies[url]) {
+        return { status: 404 };
+      }
+      return {
+        status: 200,
+        body: JSON.stringify(noEntityMockBodies[url]),
+      };
+    });
 
     render(<PageContent />);
 
     await waitFor(() => {
       expect(
-        screen.getByText(
-          "We could not find any Confluence page associated with this entity"
-        )
+        screen.getByText("No Confluence details exist on this entity.")
       ).toBeInTheDocument();
     });
   });
 
   it("shows a page if page is found", async () => {
-    fetchMock.mockResponses(
-      [JSON.stringify(serviceYaml), { status: 200 }],
-      [JSON.stringify(mockPageContent), { status: 200 }]
-    );
+    fetchMock.mockResponse(async (req) => {
+      const url = req.url.split("?")[0];
+      if (!successMockBodies[url]) {
+        return { status: 404 };
+      }
+      return {
+        status: 200,
+        body: JSON.stringify(successMockBodies[url]),
+      };
+    });
 
     render(<PageContent />);
     await waitFor(() => {
@@ -79,18 +54,24 @@ describe("PageContent", () => {
 
     await waitFor(() => {
       expect(
-        screen.getByText(
-          "We could not find any Confluence page associated with this entity"
+        screen.queryByText(
+          "To get started, please add an entity to Cortex like the following:"
         )
       ).toBeInTheDocument();
     });
   });
 
   it("renders content with dangerouslySetInnerHTML", async () => {
-    fetchMock.mockResponses(
-      [JSON.stringify(serviceYaml), { status: 200 }],
-      [JSON.stringify(mockPageContent), { status: 200 }]
-    );
+    fetchMock.mockResponse(async (req) => {
+      const url = req.url.split("?")[0];
+      if (!successMockBodies[url]) {
+        return { status: 404 };
+      }
+      return {
+        status: 200,
+        body: JSON.stringify(successMockBodies[url]),
+      };
+    });
 
     render(<PageContent />);
     await waitFor(() => {
--- plugins/confluence-plugin/src/components/PageContent.tsx
+++ plugins/confluence-plugin/src/components/PageContent.tsx
@@ -1,7 +1,5 @@
-// eslint-disable-next-line @typescript-eslint/consistent-type-imports
-import React, { useState, useEffect } from "react";
-// eslint-disable-next-line @typescript-eslint/consistent-type-imports
-// import React from "react";
+import type React from "react";
+import { useState, useEffect } from "react";
 import { isEmpty, isNil } from "lodash";
 import "../baseStyles.css";
 import {
@@ -12,16 +10,46 @@ import {
 import { getEntityYaml } from "../api/Cortex";
 import { getConfluenceDetailsFromEntity } from "../lib/parseEntity";
 
-const baseConfluenceUrl = "https://cortex-se-test.atlassian.net";
+import Instructions from "./Instructions";
 
 const PageContent: React.FC = () => {
   const [pageContent, setPageContent] = useState<string | undefined>();
   const [pageTitle, setPageTitle] = useState<string | undefined>();
   const [entityPage, setEntityPage] = useState<any | string>();
+  const [baseConfluenceUrl, setBaseConfluenceUrl] = useState<string>("");
+  const [errorStr, setErrorStr] = useState<string>("");
 
   const context = usePluginContext();
 
   useEffect(() => {
+    if (!context?.apiBaseUrl) {
+      return;
+    }
+    const getConfluencePluginConfig = async (): Promise<void> => {
+      setErrorStr("loading");
+      if (!context?.apiBaseUrl) {
+        return;
+      }
+      let newConfluenceUrl = "";
+      try {
+        const response = await fetch(
+          `${context?.apiBaseUrl}/catalog/confluence-plugin-config/openapi`
+        );
+        const data = await response.json();
+        newConfluenceUrl = data.info["x-cortex-definition"]["confluence-url"];
+      } catch (e) {}
+      setBaseConfluenceUrl(newConfluenceUrl);
+      if (!newConfluenceUrl) {
+        setErrorStr("instructions");
+      }
+    };
+    void getConfluencePluginConfig();
+  }, [context?.apiBaseUrl]);
+
+  useEffect(() => {
+    if (!context.apiBaseUrl || !context.entity?.tag || !baseConfluenceUrl) {
+      return;
+    }
     const fetchEntityYaml = async (): Promise<void> => {
       const entityTag = context.entity?.tag;
       if (!isNil(entityTag)) {
@@ -31,37 +59,79 @@ const PageContent: React.FC = () => {
             ? undefined
             : getConfluenceDetailsFromEntity(yaml);
           if (!pageID?.pageID) {
-            throw new Error("No Confluence details for entity");
+            setErrorStr("No Confluence details exist on this entity.");
+            return;
           }
           setEntityPage(pageID?.pageID);
           const jiraURL = `${baseConfluenceUrl}/wiki/rest/api/content/${pageID.pageID}?expand=body.view`;
           const contentResult = await fetch(jiraURL);
+          if (!contentResult.ok) {
+            let newErrorStr = "";
+            // if the contentResult contains valid JSON, we can use it to display an error message
+            try {
+              if (
+                contentResult.headers
+                  .get("content-type")
+                  ?.includes("application/json")
+              ) {
+                const contentJSON = await contentResult.json();
+                const msg: string =
+                  contentJSON.message || JSON.stringify(contentJSON);
+                newErrorStr = `Failed to fetch Confluence page with ID ${pageID.pageID}: ${msg}`;
+              } else {
+                // just get the text if it's not JSON
+                const contentText = await contentResult.text();
+                newErrorStr = contentText;
+              }
+            } catch (e) {
+              // if we can't parse the content, just use the status text
+              newErrorStr =
+                contentResult.statusText || "Failed to fetch Confluence page";
+            }
+            setErrorStr(newErrorStr);
+            return;
+          }
           const contentJSON = await contentResult.json();
           setPageContent(contentJSON.body.view.value);
           setPageTitle(contentJSON.title);
+          setErrorStr("");
         } catch (e) {
           // This will still result in a "We could not find any Confluence page" error in the UI, but may as well trap in console as well
+          const msg: string = e.message || e.toString();
+          setErrorStr(`Error retrieving Confluence page: ${msg}`);
           console.error("Error retrieving Confluence page: ", e);
         }
       }
     };
     void fetchEntityYaml();
-  }, [context.apiBaseUrl, context.entity?.tag]);
+  }, [context.apiBaseUrl, context.entity?.tag, baseConfluenceUrl]);
+
+  if (errorStr === "loading") {
+    return <div>Loading...</div>;
+  } else if (errorStr === "instructions") {
+    return <Instructions />;
+  } else if (errorStr) {
+    return (
+      <Box backgroundColor="light" padding={3} borderRadius={2}>
+        <Text>{errorStr}</Text>
+      </Box>
+    );
+  }
+
+  if (isNil(entityPage)) {
+    return (
+      <Box backgroundColor="light" padding={3} borderRadius={2}>
+        <Text>
+          We could not find any Confluence page associated with this entity.
+        </Text>
+      </Box>
+    );
+  }
 
   return (
     <div>
-      {isNil(entityPage) ? (
-        <Box backgroundColor="light" padding={3} borderRadius={2}>
-          <Text>
-            We could not find any Confluence page associated with this entity
-          </Text>
-        </Box>
-      ) : (
-        <div>
-          <h1 dangerouslySetInnerHTML={{ __html: pageTitle as string }}></h1>
-          <p dangerouslySetInnerHTML={{ __html: pageContent as string }}></p>
-        </div>
-      )}
+      <h1 dangerouslySetInnerHTML={{ __html: pageTitle as string }}></h1>
+      <p dangerouslySetInnerHTML={{ __html: pageContent as string }}></p>
     </div>
   );
 };
