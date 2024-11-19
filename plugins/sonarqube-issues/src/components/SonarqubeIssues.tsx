04bbe2f8c25e373e61a60bcafb19814b1c7ee861
Justin Reock <jreock@gmail.com>
2024-11-19 15:11:43 -0500

    Merge in without rebase

--- plugins/sonarqube-issues/src/components/SonarqubeIssues.tsx
+++ plugins/sonarqube-issues/src/components/SonarqubeIssues.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState, useEffect } from "react";
+import React, { useMemo, useState, useEffect, useCallback } from "react";
 import { PluginContextLocation } from "@cortexapps/plugin-core";
 import {
   SimpleTable,
