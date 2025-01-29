e6829cc39897191a5403524800ba36b946294d94
Martin Stone <martindstone@me.com>
2025-01-29 13:18:39 -0500

    fix expand button showing thru overlay

--- plugins/info-cards/src/components/InfoCardForm.tsx
+++ plugins/info-cards/src/components/InfoCardForm.tsx
@@ -61,7 +61,7 @@ export default function InfoCardForm({
     padding: "2rem",
     width: "100%",
     height: "100%",
-    zIndex: 1,
+    zIndex: 2,
   };
 
   const expandButtonStyle: React.CSSProperties = {
