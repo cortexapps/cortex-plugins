07094dad74adefcf7ba13886fa2300a3ca8037f1
Cafer Elgin <cafer.elgin@gmail.com>
2025-03-05 17:21:03 +0200

    feat: empty row acts as divider

--- plugins/info-cards/src/components/InfoRow.tsx
+++ plugins/info-cards/src/components/InfoRow.tsx
@@ -1,5 +1,5 @@
 import type { InfoRowI } from "../typings";
-import { Box } from "@chakra-ui/react";
+import { Box, theme } from "@chakra-ui/react";
 import InfoCard from "./InfoCard";
 
 interface InfoRowProps {
@@ -7,7 +7,17 @@ interface InfoRowProps {
 }
 export default function InfoRow({ infoRow }: InfoRowProps): JSX.Element {
   return (
-    <Box display={"flex"} gap={4} width={"full"} justifyContent={"center"}>
+    <Box
+      display={"flex"}
+      gap={4}
+      width={"full"}
+      justifyContent={"center"}
+      style={
+        infoRow.cards.length < 1
+          ? { height: "1px", backgroundColor: theme.colors.gray[100] }
+          : {}
+      }
+    >
       {infoRow.cards.map((card) => (
         <InfoCard key={card.id} card={card} />
       ))}
